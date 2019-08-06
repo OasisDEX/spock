96f1a95059f4aebaca2b6ceb24e71138685f1ac9
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-08-06 15:50:24 +0200

    add possibility to rewrite key used for caching

--- packages/api/middlewares/caching.ts
+++ packages/api/middlewares/caching.ts
@@ -1,22 +1,21 @@
 import { middleware as cacheMiddleware, Options as CacheOptions } from 'apicache';
-import { Application } from 'express';
-import { getLogger } from '../core/utils/logger';
-import { ApiConfig } from './config';
+import { getLogger } from '../../core/utils/logger';
+import { ApiConfig } from '../config';
+import { RequestHandler } from 'express-serve-static-core';
 
 const logger = getLogger('API/caching');
 
-export function setupCaching(app: Application, config: ApiConfig): void {
+export function caching(config: ApiConfig): RequestHandler {
   const options: CacheOptions = {
-    appendKey: (req: any) => JSON.stringify(req.body),
-    debug: true,
+    appendKey: (req: any) => config.api.responseCaching.transformKey(JSON.stringify(req.body)),
   };
 
   const middleware = cacheMiddleware(config.api.responseCaching.duration, undefined, options);
-
-  app.use(middleware);
 
   logger.info(
     'Using response cache: ',
     JSON.stringify({ duration: config.api.responseCaching.duration, ...options }),
   );
+
+  return middleware;
 }
