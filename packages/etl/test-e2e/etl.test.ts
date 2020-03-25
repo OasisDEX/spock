ce93a2559bd5e7411e65fcc1fd956bfe0ed6f1c8
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-03-25 13:05:08 +0100

    bring back last test

--- packages/etl/test-e2e/etl.test.ts
+++ packages/etl/test-e2e/etl.test.ts
@@ -1,40 +1,67 @@
-// import { join } from 'path';
-// import { pick, omit, sortBy, flatten } from 'lodash';
-// import { expect } from 'chai';
+import { join } from 'path';
+import { pick, omit, sortBy, flatten } from 'lodash';
+import { expect } from 'chai';
 
-// import { runIntegrationTest, withLocalEnv } from './common-utils';
-// import { dumpDB } from './common';
-// import { BlockTransformer } from '../src/processors/types';
+import { runIntegrationTest, withLocalEnv } from './common-utils';
+import { dumpDB } from './common';
+import { BlockTransformer, BlockExtractor } from '../src/processors/types';
+import { TransactionalServices } from '../src/types';
+import { BlockModel } from '../src/db/models/Block';
+import { Log } from 'ethers/providers';
+import { getLast } from '../src/utils';
 
-// const DAI = '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359';
+const DAI = '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359';
 
-// let allDaiData: any = [];
-// const daiTransformer: BlockTransformer = {
-//   name: 'DAI-transformer',
-//   dependencies: [getExtractorName(DAI)],
-//   transform: async (_s, data) => {
-//     const deterministicData = flatten(data).map(d => omit(d, 'tx_id'));
-//     allDaiData.push(...deterministicData);
-//   },
-// };
+let allDaiLogs: any[] = [];
 
-// describe('Spock ETL', () => {
-//   it('should work for past events', async () => {
-//     const startingBlock = 8219360;
-//     const lastBlock = startingBlock + 40;
+const simpleDaiLogExtractor: BlockExtractor = {
+  name: "simple-log-extractor",
+  async extract(services, blocks) {
+    const logs = await getLogs(services, blocks, DAI);
+    allDaiLogs.push(...logs);
+  },
+  async getData(services, blocks) {
 
-//     await withLocalEnv(join(__dirname, '../../../../'), async () => {
-//       const db = await runIntegrationTest({
-//         startingBlock,
-//         lastBlock,
-//         extractors: [...makeRawLogExtractors([DAI])],
-//         transformers: [daiTransformer],
-//         migrations: {},
-//       });
-//       const dump = await dumpDB(db);
+  }
+}
 
-//       // expect(pick(dump, ['blocks', 'extracted_logs'])).toMatchSnapshot();
-//       // expect(sortBy(allDaiData, ['block_id', 'log_index'])).toMatchSnapshot();
-//     });
-//   });
-// });
+export async function getLogs(
+  services: TransactionalServices,
+  blocks: BlockModel[],
+  address: string[] | string,
+): Promise<Log[]> {
+  if (blocks.length === 0) {
+    return [];
+  } else {
+    const fromBlock = blocks[0].number;
+    const toBlock = getLast(blocks)!.number;
+
+    return await services.provider.getLogs({
+      address,
+      fromBlock,
+      toBlock,
+    });
+  }
+}
+
+// @todo redesing this test:
+// - use transfomer
+// - put real data to database
+
+describe('Spock ETL', () => {
+  it('should work for past events', async () => {
+    const startingBlock = 8219360;
+    const lastBlock = startingBlock + 40;
+
+    await withLocalEnv(join(__dirname, '../../../'), async () => {
+      const db = await runIntegrationTest({
+        startingBlock,
+        lastBlock,
+        extractors: [simpleDaiLogExtractor],
+        transformers: [],
+        migrations: {},
+      });
+      expect(allDaiLogs.length).to.be.eq(52);
+    });
+  });
+});
