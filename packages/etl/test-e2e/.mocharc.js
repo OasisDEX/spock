2bb6fac00c055b8759faa4a66575c1d279e15174
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-03-31 21:49:03 +0200

    extract mocha options

--- packages/etl/test-e2e/.mocharc.js
+++ packages/etl/test-e2e/.mocharc.js
@@ -1,10 +1,4 @@
-process.env.NODE_ENV = 'test';
-
 module.exports = {
-  require: 'ts-node/register/transpile-only',
-  extension: ['ts'],
-  watchExtensions: ['ts'],
+  ...require('../../../.mocharc'),
   spec: ['test-e2e/**/*.test.ts'],
-  slow: 1000,
-  timeout: 10000,
 }
